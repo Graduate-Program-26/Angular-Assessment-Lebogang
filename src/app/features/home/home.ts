c46b7ee42b499f8841572f51b0183fca711181bf
LebogangMasenya <LMasenya@dvtsoftware.com>
2026-05-04 11:01:23 +0200

    fix: cleaned home page structure

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -5,9 +5,11 @@ import { PlaylistGrid } from '../playlist-view/components/playlist-grid.componen
 import { ArtistGrid } from '../artist-view/components/artist-grid.component';
 import { AlbumsGrid } from '../album-view/components/album-grid.component';
 import { TracksGrid } from '../track-view/components/track-grid.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [TopHeader, SidebarComponent, PlaylistGrid, ArtistGrid, AlbumsGrid, TracksGrid],
+  imports: [TopHeader, SidebarComponent, PlaylistGrid, ArtistGrid, AlbumsGrid, TracksGrid, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
