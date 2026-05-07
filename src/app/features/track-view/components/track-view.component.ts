d19ee989451c83fb6a93586b1c15421cb8024bae
LebogangMasenya <LMasenya@dvtsoftware.com>
2026-05-07 13:32:25 +0200

    fix: sorted out navogation between pages

--- src/app/features/track-view/components/track-view.component.ts
+++ src/app/features/track-view/components/track-view.component.ts
@@ -15,8 +15,7 @@ import { durationPipe } from '../../../shared/pipes/duration-format.pipe';
     styles: `
         .now-playing-container {
             position: fixed;
-            top: 0;
-            left: 0;
+         
             width: 100vw;
             height: 100vh;
             background-size: cover;
